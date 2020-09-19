2ec9754b317c0066d9bb5310c564877554e7f7e8
cumaniar <chiragma18633@gmail.com>
2020-09-19 20:19:08 +0530

    Removed unnecessasry imports

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,7 +10,7 @@ class Header extends Component {
       var networks = this.props.data.social.map(function (network) {
         return (
           <li key={network.name}>
-            <a href={network.url} target="_blank">
+            <a href={network.url} target="_blank" rel="noopener noreferrer">
               <i className={network.className}></i>
             </a>
           </li>
